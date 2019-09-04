3c9e28808cb3b98fa0302d13f045e512f8f91137
KimHeeRyeong <khr95039@gmail.com>
2019-09-04 17:26:15 +0900

    Feature:ServerReview

--- ServerReview/ServerReview/source.cpp
+++ ServerReview/ServerReview/source.cpp
@@ -8,18 +8,37 @@ int main() {
 	SOCKET hServer, hClient;
 	SOCKADDR_IN servAddr, clntAddr;
 
+	int szClntAddr;
+
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData))
 		ErrorMessage("WSAStartup() error");
 
-	hServer = socket(PF_INET, SOCK_STREAM,0);
+	//소켓 생성
+	hServer = socket(PF_INET, SOCK_STREAM, 0);
+	if (hServer == INVALID_SOCKET)
+		ErrorMessage("socket() error");
+
+	memset(&servAddr, 0, sizeof(servAddr));
 	servAddr.sin_family = AF_INET;
 	servAddr.sin_addr.s_addr = htonl(INADDR_ANY);//빅엔디안으로 변경
 	servAddr.sin_port = htonl(10000);
 
-	if (bind(hServer,(SOCKADDR*)&servAddr, sizeof(servAddr))==SOCKET_ERROR) {
+	if (bind(hServer, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+		ErrorMessage("bind() error");
 
-	}
+	if (listen(hServer, 5) == SOCKET_ERROR)
+		ErrorMessage("listen() error");
+
+	szClntAddr = sizeof(clntAddr);
+	hClient = accept(hServer, (SOCKADDR*)&clntAddr, &szClntAddr);
+	if (hClient == INVALID_SOCKET)
+		ErrorMessage("accept() error");
 	
+	closesocket(hClient);
+	closesocket(hServer);
+	WSACleanup();
+	system("pause");
+	return 0;
 }
 void ErrorMessage(const char* message) {
 	printf(message);
